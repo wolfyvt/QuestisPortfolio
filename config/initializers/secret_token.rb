9fd44aa5a6a154040ce01245fe26efdd23821834
Adam Wolf <adam.t.wolf@gmail.com>
2012-04-06 17:22:00 -0700

    First commit - base project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestisPortfolio::Application.config.secret_token = 'a6b9a56696fd405b485e59fcce89c1986497af8a41faf8dccaea938d7af073b097e705cf84e52e5929c161a2551b13e10fd8e80a89d8f8a94ec2a8a46e0d7f97'
